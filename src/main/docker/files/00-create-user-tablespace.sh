336575f01b1f5f798ff3edb0930a722a8a726c48
Alfred Schmid <A.Schmid@ff-muenchen.de>
2017-06-08 15:37:48 +0200

    Database flaky database initialization
    - fixed usage of database script hook, to ensure that own database
    scripts are executed after the whole database is initalized.

--- src/main/docker/files/00-create-user-tablespace.sh
+++ src/main/docker/files/00-create-user-tablespace.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-./entrypoint.sh &
 
-# Wait for oracle to start, ora_q003_xe seems to be the last process which is started and needed from oracle-xe
-until pids=$(pidof ora_q003_xe)
-do
-	sleep 1
-done
-
-# service should be up, check if orauser tablespace exists
 if [ "$(ls -A /u01/app/oracle/oradata/xe/ORAUSER.dbf)" ]; then
 	echo "found /u01/app/oracle/oradata/xe/ORAUSER.dbf -> orauser tablespace exists"
 else
@@ -15,9 +7,3 @@ else
 	su -c "$SQLPLUS '/ as sysdba' @/u01/app/oracle/create.sql" oracle
 	touch $TABLESPACE_CREATED
 fi
-
-##
-## keep container up until oracle-12c is shutdown
-##
-wait $(pgrep entrypoint.sh)
-
